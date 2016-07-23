6dfd80b0d15552116a52247a9a109f27246b5135
Tom Yang <xy2286@columbia.edu>
2016-07-22 22:25:52 -0400

    Using Convention

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,56 @@
 class ProductsController < ApplicationController
-  def product
+  def index
     @products = Product.all
+    render "index.html.erb"
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render "show.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    @product = Product.new(
+    name: params[:name],
+    price: params[:price],
+    image: params[:image],
+    description: params[:description]
+    )
+    @product.save
+    render "create.html.erb"
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render 'edit.html.erb'
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+    name: params[:name],
+    price: params[:price],
+    image: params[:image],
+    description: params[:description]
+    )
+    render "update.html.erb"
+  end
+
+  def new_form
+    render "new_form.html.erb"
+  end
+
+  def add_to_database
+    Product.create(
+    name: params[:name],
+    price: params[:price],
+    image: params[:image],
+    description: params[:description]
+    )
+    render "new_form.html.erb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  get '/products' => 'products#product'
+  get '/products' => 'products#index'
+  get '/products/new' => 'products#new'
+  post '/products'  => 'products#create'
+  get '/products/:id' => 'products#show'
+  get '/products/:id/edit' => 'products#edit'
+  patch '/products/:id'  => 'products#update'
+  get '/forms'    => 'products#new_form'
+  post '/forms'   => 'products#add_to_database'
 end
