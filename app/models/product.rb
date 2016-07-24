e356a34a9379148276b72450d12b5404600c4d7c
Tom Yang <xy2286@columbia.edu>
2016-07-24 17:01:47 -0400

    model method added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  def sale_message
+    if price.to_f < 2
+      return "On Sale !"
+    else
+      return "Not On Sale !"
+    end
+  end
+
+  def tax
+    (price.to_f * 0.1).round(2)
+  end
+
+  def total
+    (price.to_f + tax).round(2)
+  end
 end
